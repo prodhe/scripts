bf3df2b41c63e24d9462735e0e1ff86cc4e49c3b
Petter Rodhelind <petter.rodhelind@gmail.com>
2015-01-10 15:26:26 +0100

    new file: checknet.sh

---
+++ checknet/checknet.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#
+# Variables
+#
+
+dest=8.8.8.8
+src=en0
+
+pingtries=5
+threshold=3
+timer=180
+
+action="echo no connection"
+
+
+#
+# Script
+#
+
+while true; do
+    ping=$(ping -c $pingtries -I $src $dest)
+    packets=$(echo $ping | grep -o "[0-9] packets received" | awk '{print $1}')
+    if [ $packets -lt $threshold ]
+    then
+        $action
+    fi
+    sleep $timer
+done

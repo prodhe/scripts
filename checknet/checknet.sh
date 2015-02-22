a9c874e24e90be77b8e140181dc5314033548e87
Petter Rodhelind <petter.rodhelind@gmail.com>
2015-02-22 18:32:29 +0100

    documented

--- checknet/checknet.sh
+++ checknet/checknet.sh
@@ -1,29 +1,35 @@
 #!/bin/sh
 
 #
-# Variables
+# Try changing the -b flag to -I (capital i) in row 24 if your
+# ping does not work. The implementation differs from system
+# to system. Or just "man ping" and see what flag is used to
+# specify source interface.
 #
 
+
+# ping options
 dest=8.8.8.8
 src=en0
 
 pingtries=5
 threshold=3
+
+# time to wait in seconds before trying again
 timer=180
 
+# the command to execute, if the ping fails with
 action="echo no connection"
 
 
-#
-# Script
-#
+# script
 
 while true; do
-    ping=$(ping -c $pingtries -I $src $dest)
-    packets=$(echo $ping | grep -o "[0-9] packets received" | awk '{print $1}')
-    if [ $packets -lt $threshold ]
+    ping=$(ping -c "$pingtries" -b "$src" "$dest")
+    packets=$(echo "$ping" | grep -o "[0-9] packets received" | awk '{print $1}')
+    if [[ "$packets" -lt "$threshold" ]]
     then
         $action
     fi
-    sleep $timer
+    sleep "$timer"
 done
